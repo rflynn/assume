303c6035d0678b4703c488350af7155b37f5f480
pizza <pizza@parseerror.com>
2009-06-25 00:24:29 -0400

    starting to figure out how to define c99 syntax as Ocaml types.

--- doc/c/def.ml
+++ doc/c/def.ml
@@ -1,5 +1,7 @@
 (* ex: set ts=2 et: *)
 
+open List;;
+
 (* Provided by Theory *)
   
   (* type *)
@@ -22,125 +24,44 @@
   type int64    = Int64 of bitv;;
 
 (* Definition of the C Programming Language *)
-  
+
     type cident   = Ident   of string;;
     type cchar    = CChar   of char;;
+    (* this is not what we really want, as this allows the 'int'
+     * type to change during runtime. what we want is really the
+     * size of the builtin types, to be passed upon creation of 'C',
+     * which then constructs the types in a fixed manner for the
+     * remainder *)
     type cint     = CInt16  of int16
                   | CInt32  of int32
                   | CInt64  of int64;;
+
     type cfloat   = CFloat  of float;;
     type cstring  = CStr    of string;;
 
     type constant =
-        IntegerConstant   of cint
-      | FloatingConstant  of cfloat
-      | CharacterConstant of cchar
+        IntConst    of cint
+      | FloatConst  of cfloat
+      | CharConst   of cchar
       ;;
 
+    type type_name =
+        EnumName   of string
+      | StructName of string
+      | UnionName  of string
+      | TypeName   of string
+      ;;
 
-(*
-(6.5.2) postfix-expression:
-primary-expression
-postfix-expression [ expression ]
-postfix-expression ( argument-expression-listopt )
-postfix-expression . identifier
-postfix-expression -> identifier
-postfix-expression ++
-postfix-expression --
-( type-name ) { initializer-list }
-( type-name ) { initializer-list , }
+    let type_name_show tn =
+      match tn with
+        EnumName  (name) -> name
+      | StructName(name) -> name
+      | UnionName (name) -> name
+      | TypeName  (name) -> name
+      ;;
 
-(6.5.2) argument-expression-list:
-assignment-expression
-argument-expression-list , assignment-expression
-
-(6.5.3) unary-expression:
-postfix-expression
-++ unary-expression
--- unary-expression
-unary-operator cast-expression
-sizeof unary-expression
-sizeof ( type-name )
-
-(6.5.3) unary-operator: one of
-& * + - ~ !
-
-(6.5.4) cast-expression:
-unary-expression
-( type-name ) cast-expression
-
-(6.5.5) multiplicative-expression:
-cast-expression
-multiplicative-expression * cast-expression
-multiplicative-expression / cast-expression
-multiplicative-expression % cast-expression
-
-(6.5.6) additive-expression:
-multiplicative-expression
-additive-expression + multiplicative-expression
-additive-expression - multiplicative-expression
-
-(6.5.7) shift-expression:
-additive-expression
-shift-expression << additive-expression
-shift-expression >> additive-expression
-
-(6.5.8) relational-expression:
-shift-expression
-relational-expression < shift-expression
-relational-expression > shift-expression
-relational-expression <= shift-expression
-relational-expression >= shift-expression
-
-(6.5.9) equality-expression:
-relational-expression
-equality-expression == relational-expression
-equality-expression != relational-expression
-
-(6.5.10) AND-expression:
-equality-expression
-AND-expression & equality-expression
-
-(6.5.11) exclusive-OR-expression:
-AND-expression
-exclusive-OR-expression ^ AND-expression
-
-(6.5.12) inclusive-OR-expression:
-exclusive-OR-expression
-inclusive-OR-expression | exclusive-OR-expression
-
-(6.5.13) logical-AND-expression:
-inclusive-OR-expression
-logical-AND-expression && inclusive-OR-expression
-
-(6.5.14) logical-OR-expression:
-logical-AND-expression
-logical-OR-expression || logical-AND-expression
-
-(6.5.15) conditional-expression:
-logical-OR-expression
-logical-OR-expression ? expression : conditional-expression
-
-(6.5.16) assignment-expression:
-conditional-expression
-unary-expression assignment-operator assignment-expression
-
-(6.5.16) assignment-operator: one of
-= *= /= %= += -= <<= >>= &= ^= |=
-
-(6.5.17) expression:
-assignment-expression
-expression , assignment-expression
-
-(6.6) constant-expression:
-conditional-expression
-     *)
-
-    type unary_operator     = Band | Mul | Plus | Minus | Bnot | Not;;
-
-    type storage_class_specifier = Typedef | Extern | Static | Auto | Register;;
-
-    type type_specifier     =
+    type storage_class_specifier  = Typedef | Extern | Static | Auto | Register ;;
+    type type_specifier           =
         Void
       | Char   of cchar
       | Int    of cint
@@ -148,127 +69,93 @@ conditional-expression
       | Struct of cident
       | Fun    of cident
       ;;
-
-    type type_qualifier     =
-      Const | Restrict | Volatile
+    type type_qualifier           = Const | Restrict | Volatile ;;
+    type function_specifier       = Inline ;;
+    type declaration_specifier    =
+        StorClassSpec of storage_class_specifier
+      | TypeSpec      of type_specifier
+      | TypeQual      of type_qualifier
+      | FuncSpec      of function_specifier
       ;;
 
-    type function_specifier =
-      None | Inline
+    type parameter_type_list    = IdentList of cident list ;;
+
+    type unary_expression ;; (* FIXME: TODO *)
+    type conditional_expression ;; (* FIXME: TODO *)
+
+    type assignment_operator    = string;; (* NOTE: we'll leave operators as strings, for now *)
+    type assignment_expression  =
+        AssignExprCond  of conditional_expression
+      | AssignExprUnary of unary_expression * assignment_operator * assignment_expression
       ;;
 
-    type op = string;;
-
-    (*
-     * hmm, can't really use the standard's definition,
-     * BNF-style syntax doesn't work in OCaml because Ocaml
-     * can't do forward references of types
-     *)
-    type expr =
-        Constant of string
-      | Variable of string
-      | Unary    of op * expr
-      | Cast     of string * expr
-      | Binary   of expr * op * expr
-      | Assign   of expr * op * expr
-      | Ternary  of expr * expr * expr
+    type pointer ;; (* FIXME: TODO *)
+    type direct_declarator      =
+        DDIdent       of cident
+      | DDDeclarator  of declarator
+      | DDDDArray     of direct_declarator * type_qualifier list * assignment_expression (* TODO: static *)
+      | DDDDFuncParam of parameter_type_list
+      | DDDDFuncIdent of cident list
+    and declarator              =
+        Declarator    of pointer * direct_declarator
       ;;
 
-    let rec show = function
-      | Constant(c)  -> c
-      | Variable(v)  -> v
-      | Unary(op, e) -> op ^ show(e)
-      | Cast(typ, e) -> "((" ^ typ ^ ")" ^ show(e) ^ ")"
-      | Binary(a, "+", b) -> "(" ^ show(a) ^ " + " ^ show(b) ^ ")"
-      | Binary(a, op, b) -> "(" ^ show(a) ^ " " ^ op ^ " " ^ show(b) ^ ")"
-      | Assign(dst, op, src) -> show(dst) ^ " " ^ op ^ " " ^ show(src)
-      | Ternary(e, iftrue, iffalse) -> "(" ^ show(e) ^ " ? " ^ show(iftrue) ^ " : " ^ show(iffalse) ^ ")"
+    type initializer_           =
+        InitializerAssign   of assignment_expression
+      | Initializer         of initializer_ list
+      ;;
+    type init_declarator        =
+        InitDeclarator      of declarator
+      | InitDeclaratorInit  of initializer_
+    and  declaration            =
+      Declaration           of declaration_specifier list * init_declarator list
       ;;
 
-    (*
-    type binary_expression =
-      
+(*
+(6.7.8) initializer:
+                assignment-expression
+                { initializer-list }
+                { initializer-list , }
+(6.7.8) initializer-list:
+                designationopt initializer
+                initializer-list , designationopt initializer
+(6.7.8) designation:
+                designator-list =
+(6.7.8) designator-list:
+                designator
+                designator-list designator
 
-    type assignment_expression =
-      AssignmentExpr unary_expression * assignment_operator * conditional_expression
-      ;;
-
-    type expression =
-      ExprAssignment assignment_expression
-      ;;
-
-    type conditional_expression =
-      CondExpr expression
-      | Ternary expression * conditional_expression
-      ;;
-
-    type primary_expression =
-        PriExprIdent of cidentifier
-      | PriExprConst of constant
-      | PriExprStr   of cstring
-      | PriExprExpr  of expression
-      ;;
-    *)
-
-
-
-    
-
-    (* A.2.3 Statements *)
-    (*
-    type statement = 
-      labeled_statement
-      | compound_statement
-      | expression_statement
-      | selection_statement
-      | iteration_statement
-      | jump_statement
-      ;;
-
-    type labeled_statement =
-      Label_Stmt_Ident of identifier * statement
-      | Case constant_expression * statement
-      | Default statement
-      ;;
-
-    type compound-statement =
-      Block_Item_List [block_item]
-      ;;
-    *)
-    (*
-
-    type block-item:
-declaration
-statement
-(6.8.3) expression-statement:
-expressionopt ;
-(6.8.4) selection-statement:
-if ( expression ) statement
-if ( expression ) statement else statement
-switch ( expression ) statement
-(6.8.5) iteration-statement:
-while ( expression ) statement
-do statement while ( expression ) ;
-for ( expressionopt ; expressionopt ; expressionopt ) statement
-for ( declaration expressionopt ; expressionopt ) statement
-
-    type 
-
-    *)
-
-    (* A.2.4 External definitions *)
-    (*
-    type translation_unit = 
-      Tran_Unit of [external_declaration]
-      ;;
-
-    type external_declaration =
-      Ext_Dec_Fun function_definition
-      | Ext_Decl declaration
-      ;;
-
-    type function_definition =
-      Function_Definition declaration_specifiers declarator [declaration] compound_statement
-      ;;
-    *)
+(6.7) init-declarator:
+               declarator
+               declarator = initializer
+(6.7.5) declarator:
+               pointeropt direct-declarator
+(6.7.5) direct-declarator:
+               identifier
+               ( declarator )
+               direct-declarator [ type-qualifier-listopt assignment-expressionopt ]
+               direct-declarator [ static type-qualifier-listopt assignment-expression ]
+               direct-declarator [ type-qualifier-list static assignment-expression ]
+               direct-declarator [ type-qualifier-listopt * ]
+               direct-declarator ( parameter-type-list )
+               direct-declarator ( identifier-listopt )
+(6.7.5) pointer:
+               * type-qualifier-listopt
+               * type-qualifier-listopt pointer
+(6.7.5) type-qualifier-list:
+               type-qualifier
+               type-qualifier-list type-qualifier
+(6.7.5) parameter-type-list:
+               parameter-list
+               parameter-list , ...
+(6.7.5) parameter-list:
+               parameter-declaration
+               parameter-list , parameter-declaration
+(6.7.5) parameter-declaration:
+               declaration-specifiers declarator
+               declaration-specifiers abstract-declaratoropt
+(6.7.5) identifier-list:
+               identifier
+               identifier-list , identifier
+*)
 
