5e05e25e8fc89d608c1d711c1d7a099b6c72331d
pizza <pizza@parseerror.com>
2009-06-26 00:57:20 -0400

    made some good progress with programming-language-model; added some graphviz files to help me work on my model. moved c-specific bugdb under lang/c99, makes sense.

--- lang/bin-arith/gramgen.erl
+++ lang/bin-arith/gramgen.erl
@@ -1,25 +1,48 @@
--module(gramgen).
--export([combo/2, contains/2, test/0]).
+% 
+% given a symbolic grammar, generate valid input for it
+% 
 
-% exposed interface
-combo(_, 0) -> [[]];
-combo([], _) -> [[]];
-combo(L, Cnt) -> combo(L, Cnt, []).
+-module(gramgen).
+-export([combo/2, choose/1, test_perm/0, contains/2]).
 
 % c(gramgen), l(gramgen).
-% gramgen:combo([1,2,3],1).
+% gramgen:combo([1,2,3], 2).
 
-% real function, accumulator
-combo([],        _, Acc) -> [Acc];
-combo(_,         0, Acc) -> [Acc];
-combo([H|T],   Cnt, Acc) ->
-  combo(T, Cnt, Acc) ++
-  combo(T, Cnt, Acc ++ [H]).
+combo([], _) -> [[]];
+combo(L, Max) -> combo([], L, Max).
+combo(Acc, [],    _) -> [Acc];
+combo(_,     0, Acc) -> [Acc];
+combo(Acc, Max, [H|T]) ->
+  combo(T, Max-1, Acc) ++
+  combo(T, Max-1, Acc ++ [H]).
+
+choose([]) -> [[]];
+choose(L)  -> choose([], L).
+
+choose(Acc, []) -> [Acc];
+choose(Acc, [H|T]) ->
+  choose(Acc ++ [H], T) ++ % take it
+  choose(Acc, T).          % don't
+
+test_choose() ->
+  test_choose(0).
+test_choose(6) -> true;
+test_choose(N) when N >= 0 ->
+  Len = length(choose(lists:seq(0,N))),
+  Expect = math:pow(2, N),
+  if
+    Len /= Expect ->
+      io:format("Shit! N=~p Expect=~p L=~p~n", [N,Expect,Len]),
+      exit(shit);
+    true ->
+      test_choose(N+1)
+  end.
 
 contains([], _) -> false;
 contains([Foo|_], Foo) -> true;
 contains([_|T], Foo) -> contains(T, Foo).
 
-test() ->
-  combo([1,2], 2).
+fac(0) -> 0;
+fac(1) -> 1;
+fac(N) when N > 1 -> N * fac(N-1).
 
